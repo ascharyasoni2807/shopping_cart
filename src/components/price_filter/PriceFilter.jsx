0c37d6fe565058dfd1e528ba2b0b995ed05e757c
ascharyasoni2807 <65384919+ascharyasoni2807@users.noreply.github.com>
2024-05-22 02:25:00 +0530

    Feat: Added price range filter

---
+++ src/components/price_filter/PriceFilter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+
+const PriceFilter = ({ handlePriceRangeChange }) => {
+  const handleOnChange = (eventKey) => {
+    setSelectedRange(eventKey);
+    handlePriceRangeChange(eventKey);
+  };
+  const [selectedRange, setSelectedRange] = useState("");
+  return (
+    <DropdownButton
+      id="dropdown"
+      title={selectedRange ? selectedRange : "Price Range"}
+      onSelect={handleOnChange}
+    >
+      <Dropdown.Item eventKey="">All Prices</Dropdown.Item>
+      <Dropdown.Item eventKey="100-500">$100 - $500</Dropdown.Item>
+      <Dropdown.Item eventKey="500-1000">$500 - $1000</Dropdown.Item>
+      <Dropdown.Item eventKey="1000-2000">$1000 - $2000</Dropdown.Item>
+      <Dropdown.Item eventKey="2000">$2000 and above</Dropdown.Item>
+    </DropdownButton>
+  );
+};
+
+export default PriceFilter;
--- src/pages/products/ProductList.jsx
+++ src/pages/products/ProductList.jsx
@@ -23,6 +23,7 @@ import {
 } from "../../constants/constant";
 import "./ProductList.css";
 import LoadingSpinner from "../../components/custom_spinner/LoadingSpinner";
+import PriceFilter from "../../components/price_filter/PriceFilter";
 
 const ProductList = ({
   products,
@@ -57,16 +58,36 @@ const ProductList = ({
   const handleCategoryChange = (categoryId) => {
     setSelectedCategory(categoryId);
   };
+  const [selectedPriceRange, setSelectedPriceRange] = useState("");
+
+  const handlePriceRangeChange = (priceRange) => {
+    setSelectedPriceRange(priceRange);
+  };
+
+  const filterProductsByPriceRange = (range) => {
+    const [min, max] = range.split("-").map(Number);
+    if (max === undefined) {
+      return products.filter((product) => product.price >= min);
+    } else {
+      return products.filter(
+        (product) => product.price >= min && product.price <= max
+      );
+    }
+  };
+
+  let filteredProducts = products;
+  if (selectedPriceRange) {
+    filteredProducts =
+      selectedPriceRange === "2000"
+        ? filterProductsByPriceRange("2000-")
+        : filterProductsByPriceRange(selectedPriceRange);
+  }
 
   const totalCartItems = cart?.reduce(
     (total, product) => total + product.quantity,
     0
   );
 
-  console.log(categories);
-  const filteredProducts = selectedCategory
-    ? products.filter((product) => product.category === selectedCategory)
-    : products;
   return (
     <div>
       <div
@@ -74,6 +95,9 @@ const ProductList = ({
       >
         <h2>{PRODUCT_LIST}</h2>
         <Row>
+          <Col>
+            <PriceFilter handlePriceRangeChange={handlePriceRangeChange} />
+          </Col>
           <Col>
             <DropdownButton
               id="dropdown"
